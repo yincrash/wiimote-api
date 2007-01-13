7f188f44b3dedc8a097c64c70686009467ab89cc
yincrash <yincrash@4f1d8b47-aa26-0410-a61f-4dae3a6d859b>
2007-01-13 08:25:06 +0000

    added a couple of new functions. check the header file.

--- src/wiimote-api.c
+++ src/wiimote-api.c
@@ -10,12 +10,15 @@
 void WShowError2(LPCTSTR, LPCTSTR);
 void WShowError(LPCTSTR);
 void ParseReport(unsigned char*);
+int LoopAction(LPBYTE, BOOL);
 
 /* current controller status */
 HANDLE wiimotehandle;
 BYTE led[4] = {0, 0, 0, 0};
 BYTE rumble = 0;
 BYTE ir     = 0;
+WIIMOTE_MAP wiimote_keys;
+WIIMOTE_MAP wiimote_status;
 
 
 void WShowError(LPCTSTR title)
@@ -32,7 +35,7 @@ void WShowError2(LPCTSTR title, LPCTSTR e_msg)
 	}
 	else
 	{
-		sprintf(buffer, "Error Code: %d\nCheck http://msdn.microsoft.com/library/default.asp?url=/library/en-us/debug/base/system_error_codes.asp", GetLastError());
+		sprintf(buffer, "wiimote-api:\nError Code: %d\nCheck http://msdn.microsoft.com/library/default.asp?url=/library/en-us/debug/base/system_error_codes.asp", GetLastError());
 		MessageBox(NULL, buffer, title, MB_OK | MB_ICONERROR);
 	}
 }
@@ -72,17 +75,36 @@ sample report...
 */
 void ParseReport(unsigned char* report)
 {
-	INPUT input[1];
+	INPUT mouse_input[1];
+	INPUT remotebuttons[11];
 	MOUSEINPUT* mouse;
+	KEYBDINPUT* keybd;
 	static rbutton = 0;
 	static lbutton = 0;
 	unsigned char x_mult = 0;
 	unsigned char y_mult = 0;
+	int index;
+	UINT num_buttons = 0;
+	LPBYTE status;
+	BYTE button_mask;
+	int byte;
+	/* status information */
+	if(report[0] == 0x20)
+		return;
+	/* read memory data */
+	if(report[0] == 0x21)
+		return;
+	/* write memory data status */
+	if(report[0] == 0x22)
+		return;
+	/* extension data only */
+	if(report[0] == 0x3d)
+		return;
 	 /* IR/Motion/Button report, plus in range */
 	if(report[0] == 0x33 && report[8] != 0xFF)
 	{
-		input->type = INPUT_MOUSE;
-		mouse = &(input->mi);
+		mouse_input->type = INPUT_MOUSE;
+		mouse = &(mouse_input->mi);
 		mouse->dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
 		/* use one dot */
 		x_mult = (report[8] & 0x30) >> 4;
@@ -116,10 +138,110 @@ void ParseReport(unsigned char* report)
 			mouse->dwFlags |= MOUSEEVENTF_RIGHTUP;	
 			rbutton = 0;
 		}
-		if(SendInput(1, input, sizeof(input[1])) == 0 )
+		if(SendInput(1, mouse_input, sizeof(INPUT)) == 0 )
 			WShowError("Pointer to Mouse");
 
 	}
+	/* parse buttons */
+	/* a button mapped to 0 means no mapping */
+	/* TODO */
+	for(index = 0; index < 11; index++)
+	{
+		remotebuttons[num_buttons].type = INPUT_KEYBOARD;
+		keybd = &(remotebuttons[num_buttons].ki);
+		switch(index)
+		{
+			/* case numbering is irrelevant, just unique */
+			case 0: /* up */
+				button_mask = 0x08;
+				byte = 1;
+				keybd->wVk = (WORD) wiimote_keys.up;
+				status = &(wiimote_status.up);
+				break;
+			case 1: /* left */
+				button_mask = 0x01;
+				byte = 1;
+				keybd->wVk = (WORD) wiimote_keys.left;
+				status = &(wiimote_status.left);
+				break;
+			case 2: /* right */
+				button_mask = 0x02;
+				byte = 1;
+				keybd->wVk = (WORD) wiimote_keys.right;
+				status = &(wiimote_status.right);
+				break;
+			case 3: /* down */
+				button_mask = 0x04;
+				byte = 1;
+				keybd->wVk = (WORD) wiimote_keys.down;
+				status = &(wiimote_status.down);
+				break;
+			case 4: /* a */
+				button_mask = 0x08;
+				byte = 2;
+				keybd->wVk = (WORD) wiimote_keys.a;
+				status = &(wiimote_status.a);
+				break;
+			case 5: /* b */
+				button_mask = 0x04;
+				byte = 2;
+				keybd->wVk = (WORD) wiimote_keys.b;
+				status = &(wiimote_status.b);
+				break;
+			case 6: /* minus */
+				button_mask = 0x10;
+				byte = 2;
+				keybd->wVk = (WORD) wiimote_keys.minus;
+				status = &(wiimote_status.minus);
+				break;
+			case 7: /* home */
+				button_mask = 0x80;
+				byte = 2;
+				keybd->wVk = (WORD) wiimote_keys.home;
+				status = &(wiimote_status.home);
+				break;
+			case 8: /* plus */
+				button_mask = 0x10;
+				byte = 1;
+				keybd->wVk = (WORD) wiimote_keys.plus;
+				status = &(wiimote_status.plus);
+				break;
+			case 9: /* one */
+				button_mask = 0x02;
+				byte = 2;
+				keybd->wVk = (WORD) wiimote_keys.one;
+				status = &(wiimote_status.one);
+				break;
+			case 10: /* two */
+				button_mask = 0x01;
+				byte = 2;
+				keybd->wVk = (WORD) wiimote_keys.two;
+				status = &(wiimote_status.two);
+				break;
+			default: /* shouldn't happen, but just in case */
+				keybd->wVk = 0;
+		}
+		if(keybd->wVk == 0) /* no key mapping */
+			continue;
+		keybd->time = 0;
+		keybd->wScan = 0;
+		if(report[byte] & button_mask) /* key down */
+		{
+			*status = 1;
+			keybd->dwFlags = 0;
+		}
+		else /* key up */
+		{
+			if(*status == 0) /* key already up */
+				continue;
+			*status = 0;
+			keybd->dwFlags = KEYEVENTF_KEYUP;
+		}
+		keybd->dwExtraInfo = GetMessageExtraInfo();
+		num_buttons++;
+	}
+	if(SendInput(num_buttons, remotebuttons, sizeof(INPUT)) == 0 )
+		WShowError("Wii Remote Buttons");
 }
 
 /*
@@ -128,31 +250,37 @@ void ParseReport(unsigned char* report)
  */
 int WiiM_ConnectWiimote(LPBYTE devicepath)
 {
-	OVERLAPPED hidoverlap;
-#if defined(_WIN64)
-	INT64 index;
-#else
-	int index;
-#endif
-	/* event that can be signaled */
-	hidoverlap.hEvent = CreateEvent(NULL, TRUE, TRUE, ""); 
-	hidoverlap.Offset = 0;
-	hidoverlap.OffsetHigh = 0;
 	/* handle for just reading, currently */
 	wiimotehandle = CreateFile(devicepath, GENERIC_READ | GENERIC_WRITE,
 			FILE_SHARE_READ | FILE_SHARE_WRITE, 
 			NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL);
 	if(wiimotehandle == INVALID_HANDLE_VALUE)
 	{
-		printf("Error Code: %d\n", GetLastError());
-		printf("Check http://msdn.microsoft.com/library/default.asp?url=/library/en-us/debug/base/system_error_codes.asp\n");
+		WShowError("While Connecting to Wiimote");
 	}
+	memset(&wiimote_keys, 0, sizeof(WIIMOTE_MAP));
+	memset(&wiimote_status, 0, sizeof(WIIMOTE_MAP));
 	return 1;
 }
 
 void WiiM_CloseConn()
 {
 	CloseHandle(wiimotehandle);
+}
+
+/* sets a new key map for the wiimote, if NULL is passed, 
+ * previous map is cleared */
+void WiiM_SetWiimoteKeyMap(WIIMOTE_MAP* keys)
+{
+	if(keys == NULL)
+		memset(&wiimote_keys, 0, sizeof(WIIMOTE_MAP));
+	else
+		memcpy(&wiimote_keys, keys, sizeof(WIIMOTE_MAP));
+}
+
+WIIMOTE_MAP WiiM_GetWiimoteKeyStatus()
+{
+	return wiimote_status;
 }
 
 void WiiM_TogRumble()
@@ -190,7 +318,7 @@ void WiiM_TogIR_Abs()
 	/* enabled using marcan's info */
 	ir = !ir;
 	if(ir) {
-		/* set mode to motion + IR */
+		/* set mode to motion + (basic) IR */
 		report[0] = 0x12;
 		report[1] = 0x00;
 		report[2] = 0x33;
@@ -232,17 +360,29 @@ void WiiM_TogIR_Abs()
 	}
 }
 
+/* buffer needs to hold at least 22 characters */
+int WiiM_GetReport(LPBYTE buffer)
+{
+	return LoopAction(buffer, FALSE);
+}
+
 int WiiM_ProcessEvent()
 {
-	return WiiM_ProcessAndGetReport(NULL);
+	return LoopAction(NULL, TRUE);
 }
 
 /* buffer needs to hold at least 22 characters */
 int WiiM_ProcessAndGetReport(LPBYTE buffer)
 {
+	return LoopAction(buffer, TRUE);	
+}
+
+int LoopAction(LPBYTE buffer, BOOL process)
+{
 	OVERLAPPED hidoverlap;
 	DWORD result;
 	PDWORD size = malloc(sizeof(DWORD));
+	BOOL nobuffer = FALSE;
 
 	/* event that can be signaled */
 	hidoverlap.hEvent = CreateEvent(NULL, TRUE, TRUE, ""); 
@@ -250,7 +390,10 @@ int WiiM_ProcessAndGetReport(LPBYTE buffer)
 	hidoverlap.OffsetHigh = 0;
 
 	if(buffer == NULL)
+	{
 		buffer = malloc(22 * sizeof(BYTE));
+		nobuffer = TRUE;
+	}
 
 	if(!ReadFile(wiimotehandle, buffer, 22, size, &hidoverlap))
 	{
@@ -276,7 +419,10 @@ int WiiM_ProcessAndGetReport(LPBYTE buffer)
 		}
 	}
 	ResetEvent(hidoverlap.hEvent);
-	ParseReport(buffer);
+	if(process)
+		ParseReport(buffer);
+	if(nobuffer)
+		free(buffer);
 
 	free(size);
 	return 1;
